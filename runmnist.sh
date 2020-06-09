8a6699f392b1a6e524799aa400a3324f480a988b
Ignacio <ignaciocobas@outlook.com>
2020-06-10 01:28:11 +0200

    runmnist.sh now downloads scipy and pip3 if necessary

--- runmnist.sh
+++ runmnist.sh
@@ -1,5 +1,17 @@
 #!/usr/bin/env bash
-pip3 install scipy
+vscipy=$(pip show scipy)
+if [ "$vscipy" == '' ];
+then
+  echo "scipy not installed"
+  vpip=$(pip3 -V)
+  if [ "$vpip" == '' ];
+  then
+    echo "pip3 not installed"
+    apt update;
+    apt install python3-pip;
+  fi
+  pip3 install scipy
+fi
 
 python3 mnist.py
 python3 ClientMNIST.py
